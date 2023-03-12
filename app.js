15afc1ea9f92df57ea5e60807e43a74b0be9e816
000-987 <usphadkfj159@gmail.com>
2023-03-12 09:50:26 +0800

    靜態網頁樣式

---
+++ app.js
@@ -0,0 +1,28 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/restaurants/1', (req, res) => {
+  res.render('show')
+})
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+
